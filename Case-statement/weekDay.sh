6ed2960cf8e7bd7a913e12a9829c42998c8008d7
SidK29 <kharadesiddhesh96@gmail.com>
2021-02-23 22:37:03 +0530

    Added: Read no. and display weekday

---
+++ Case-statement/weekDay.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+echo "Enter weekday number (0-7): "
+read a
+
+case $a in
+	0) echo "Sunday" ;;
+	1) echo "Monday" ;;
+	2) echo "Tuesday" ;;
+	3) echo "Wednesday" ;;
+	4) echo "Thursday" ;;
+	5) echo "Friday" ;;
+	6) echo "Saturday";;
+esac
+
